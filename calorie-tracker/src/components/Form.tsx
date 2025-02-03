d2723739b3e8b217e07c752fa63b8fc787f92f56
Antonio-Delarua <ruxxdeveloper@gmail.com>
2025-02-03 14:08:13 +0100

    finish contador de calorias

--- calorie-tracker/src/components/Form.tsx
+++ calorie-tracker/src/components/Form.tsx
@@ -1,24 +1,70 @@
-import { useState } from "react"
+import { useState, ChangeEvent, FormEvent, Dispatch, useEffect } from "react"
+import { v4 as uuidv4} from 'uuid'
 import { categories } from "../data/categories"
+import type { Activity} from "../types"
+import { ActivityActions, ActivityState } from "../reducers/activity-reducer"
 
-export default function Form() {
+type FormProps = {
+    dispatch: Dispatch<ActivityActions>,
+    state: ActivityState
+}
 
-    const [activity, setActivity] = useState({
-        category: 1,
-        name: '',
-        calories: 0
-    })
-    
+const initialState: Activity = {
+    id: uuidv4(),
+    category: 1,
+    name: '',
+    calories: 0
+}
+
+export default function Form({dispatch, state}: FormProps) {
+
+    const [activity, setActivity] = useState<Activity>(initialState)
+
+    useEffect(() => {
+        if(state.activeId){
+            const selectedActivity = state.activities.filter( stateActivity => stateActivity.id === state.activeId)[0]
+            setActivity(selectedActivity)
+        }
+    }, [state.activeId])
+
+    const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+        const isNumberField = ['category', 'calories'].includes(e.target.id)
+
+        setActivity({
+            ...activity,
+            [e.target.id]: isNumberField ? +e.target.value : e.target.value
+        })
+        console.log(e.target.id)
+        console.log(e.target.value)
+    }
+
+    const isValidActivity = () => {
+        const {name, calories} = activity
+        return name.trim() !== '' && calories > 0
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        dispatch({
+            type: 'save-activity',payload: {newActivity: activity}})
+            setActivity({
+                ...initialState,
+                id: uuidv4(),
+            })
+    }
 
   return (
     <form
-    className="space-y-5 bg-white shadow p-10 rounded-lg ">
+    className="space-y-5 bg-white shadow p-10 rounded-lg "
+    onSubmit={handleSubmit}
+    >
         <div className="grid grid-cols-1 gap-3">
             <label htmlFor="category" className="font-bold">Categoria:</label>
             <select
             className="border border-slate-300 p-2 rounded-lg w-full bg-white"
             id="category"
             value={activity.category}
+            onChange={handleChange}
             >
                 {categories.map((category: Category) => (
                     <option key={category.id}
@@ -38,6 +84,7 @@ export default function Form() {
              className="border border-slate-300 p-2 rounded-lg"
              placeholder="Ej. Comida, Coca Cola, Ensalada, Ejercicio, etc.."
              value={activity.name}
+             onChange={handleChange}
             />
         </div>
 
@@ -49,13 +96,15 @@ export default function Form() {
              className="border border-slate-300 p-2 rounded-lg"
              placeholder="Calorias, ej. 100, 200, 300, etc.."
              value={activity.calories}
+             onChange={handleChange}
             />
         </div>
 
         <input 
         type="submit" 
-        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer rounded-lg"
-        value="guardar comida o guardar ejercicio"
+        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer rounded-lg disabled:opacity-10"
+        value={activity.category === 1 ? 'Guardar Comida' : 'Guardar Ejercicio'}
+        disabled={!isValidActivity()}
         
         />
 
